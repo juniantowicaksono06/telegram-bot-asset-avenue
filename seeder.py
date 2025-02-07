957a98f423ae0d886e049aebf88e7143bba70e99
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2025-02-07 17:04:13 +0700

    Update seeder REFERRAL_LINK_ACTIVE_DAYS

--- seeder.py
+++ seeder.py
@@ -10,7 +10,7 @@ def seed_config():
     rows = query("SELECT * FROM bot_config", dictionary=True)
     if rows:
         return
-    command("INSERT INTO bot_config (MESSAGE_POINTS, MEDIA_POINTS, MAX_MESSAGE_POINTS, MAX_MEDIA_POINTS, REFERRED_MIN_ACTIVATION, REFERRAL_ACTIVE_DAYS, REFERRAL_POINTS, REFERRAL_LINK_ACTIVE_DAYS) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (100, 200, 700, 400 ,3, 3, 200, 3))
+    command("INSERT INTO bot_config (MESSAGE_POINTS, MEDIA_POINTS, MAX_MESSAGE_POINTS, MAX_MEDIA_POINTS, REFERRED_MIN_ACTIVATION, REFERRAL_ACTIVE_DAYS, REFERRAL_POINTS, REFERRAL_LINK_ACTIVE_DAYS) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (100, 200, 700, 400 ,3, 3, 200, 10))
 
 if __name__ == "__main__":
     seed_config()
